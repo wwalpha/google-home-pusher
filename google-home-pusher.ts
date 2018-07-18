ebb0916d64f3e724f499bad16de86e71ca9813cf
wwalpha <wwalpha@gmail.com>
2018-07-18 17:29:45 +0900

    Update google-home-pusher.ts

--- google-home-pusher.ts
+++ google-home-pusher.ts
@@ -1,119 +1,114 @@
-const multicastDNS = require('multicast-dns');
+import multicastDNS from 'multicast-dns';
+import { Client, DefaultMediaReceiver } from 'castv2-client';
+import googletts from 'google-tts-api';
 
-const Client = require('castv2-client').Client;
-const DefaultMediaReceiver = require('castv2-client').DefaultMediaReceiver;
-
-let deviceAddress: string;
 let language: string;
+let deviceAddress: string;
 let deviceName: string;
+let languageAccent: string = 'us';
 
-const getPlayUrl = (url: string, host: string, callback: any) => {
-  onDeviceUp(host, url, (res: any) => {
-    callback(res);
-  });
-};
-
-export const device = (name: string, lang: string = 'jp') => {
+export const device = (name: string, lang: string = 'en') => {
   deviceName = name;
   language = lang;
   return this;
 };
 
-export const ip = (ip: string, lang: string = 'jp') => {
+export const ip = (ip: string, lang: string = 'en') => {
   deviceAddress = ip;
+  language = lang;
   return this;
 };
 
-const googletts = require('google-tts-api');
-let googlettsaccent = 'us';
-// 國語 (台灣)	cmn-Hant-TW	Chinese, Mandarin (Traditional, Taiwan)
-// 廣東話 (香港)	yue-Hant-HK	Chinese, Cantonese (Traditional, Hong Kong)
-// 日本語（日本）	ja-JP	Japanese (Japan)
-// 普通話 (香港)	cmn-Hans-HK	Chinese, Mandarin (Simplified, Hong Kong)
-// 普通话 (中国大陆)	cmn-Hans-CN	Chinese, Mandarin (Simplified, China)
-const accent = (accent: string) => {
-  googlettsaccent = accent;
+// https://cloud.google.com/speech-to-text/docs/languages
+export const accent = (accent: string = 'us') => {
+  languageAccent = accent;
   return this;
 };
 
-const notify = (message: string, callback: any) => {
-  if (!deviceAddress) {
-    const mdns = multicastDNS();
-    mdns.on('response', (service: any) => {
-      console.log('Device "%s" at %s:%d', service.name, service.addresses[0], service.port);
-      if (service.name.includes(deviceName.replace(' ', '-'))) {
-        deviceAddress = service.addresses[0];
-        getSpeechUrl(message, deviceAddress, (res: any) => {
-          callback(res);
-        });
-      }
-      mdns.destroy();
-    });
-  } else {
-    getSpeechUrl(message, deviceAddress, (res: any) => {
-      callback(res);
-    });
-  }
-};
+const initAddress = () => {
+  const mdns = multicastDNS();
 
-const play = (mp3Url: string, callback: any) => {
-  if (!deviceAddress) {
-    const mdns = multicastDNS();
-    mdns.on('response', (service: any) => {
-      console.log('Device "%s" at %s:%d', service.name, service.addresses[0], service.port);
-      if (service.name.includes(deviceName.replace(' ', '-'))) {
-        deviceAddress = service.addresses[0];
-        getPlayUrl(mp3Url, deviceAddress, (res: any) => {
-          callback(res);
-        });
-      }
-      mdns.destroy();
-    });
-  } else {
-    getPlayUrl(mp3Url, deviceAddress, (res: any) => {
-      callback(res);
-    });
-  }
-};
+  mdns.on('response', (res: any) => {
+    console.log('response:', res);
+    const { additionals = [] } = res;
 
-const getSpeechUrl = (text: string, host: string, callback: any) => {
-  googletts(text, language, 1).then((url: string) => {
-    onDeviceUp(host, url, (res: any) => {
-      callback(res);
-    });
-  }).catch((err: any) => {
-    console.error(err.stack);
+    const device = additionals.find((item: any) => item.type === 'A' && item.name === `${deviceName}.local`);
+
+    if (device) {
+      deviceAddress = device.data;
+    } else {
+      console.log('Can not find any device by given name.');
+    }
+
+    mdns.destroy();
+  });
+
+  mdns.query({
+    questions: [{
+      name: '_googlecast._tcp.local',
+      type: 'PTR',
+    }],
   });
 };
 
-
-
-export const onDeviceUp = (host: string, url: string, callback: any) => {
+const onDeviceUp = (url: string, host: string): Promise<any> => new Promise((resolve, reject) => {
   const client = new Client();
+
   client.connect(host, () => {
+    console.log('connected, launching app ...');
+
     client.launch(DefaultMediaReceiver, (err: any, player: any) => {
+      if (err) {
+        reject(err);
+        return;
+      }
 
       const media = {
         contentId: url,
         contentType: 'audio/mp3',
-        streamType: 'BUFFERED', // or LIVE
+        streamType: 'BUFFERED',
       };
+
+      player.on('status', (status: any) => {
+        // ????
+        console.log('status broadcast playerState=%s', status.playerState);
+      });
+
       player.load(media, { autoplay: true }, (err: any, status: any) => {
+        console.log('media loaded playerState=%s', status.playerState);
+        // ????
         client.close();
-        callback('Device notified');
+        resolve();
       });
     });
   });
 
   client.on('error', (err: any) => {
-    console.log('Error: %s', err.message);
     client.close();
-    callback('error');
+    reject(err);
   });
+});
+
+const playSpeech = async (text: string, host: string) => {
+  const url = await googletts(text, language, 1);
+
+  await onDeviceUp(host, url);
 };
 
-// exports.ip = ip;
-// exports.device = device;
-// exports.accent = accent;
-// exports.notify = notify;
-// exports.play = play;
+/** Play google speech */
+export const notify = async (message: string) => {
+  if (!deviceAddress) {
+    initAddress();
+  }
+
+  return await playSpeech(message, deviceAddress);
+};
+
+/** Play wave */
+export const play = async (mp3Url: string) => {
+  if (!deviceAddress) {
+    initAddress();
+  }
+
+  return await onDeviceUp(mp3Url, deviceAddress);
+};
